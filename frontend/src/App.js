931ef44a933154d14dc8976eab2f66e776498a55
Alfonso Falcone <alfonso.falcone@webness.it>
2022-04-14 09:10:59 +0200

    Added template for products in homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./templates/HomeScreen";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className="main-content"></main>
+      <main className="main-content">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
       <Footer />
     </>
   );
---
+++ frontend/src/templates/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import ProductCard from "../components/ProductCard";
+import products from "../products";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map(
+          (product) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <ProductCard product={product} />
+            </Col>
+          )
+          // Il risultato del map qui sopra è l'equivalente di una arrow function che esegue il return del component card
+          // Il component card restituito utilizza le informazioni passate dal parametro product
+          // (product) => {
+          //     return (
+          //     <Col sm={12} md={6} lg={4} xl={3}>
+          //         <ProductCard product={product} />
+          //     </Col>
+          //     )
+          // }
+        )}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
