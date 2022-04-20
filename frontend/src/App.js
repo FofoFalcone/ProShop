e9037339c4c4fb80814d3dba440939e4e1dbf42f
Alfonso Falcone <alfonso.falcone@webness.it>
2022-04-20 08:32:48 +0200

    React Router 6 and creating routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./templates/HomeScreen";
-import { Container } from "react-bootstrap";
+import ProductScreen from "./templates/ProductScreen";
+import CartScreen from "./templates/CartScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="main-content">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={< HomeScreen />} />
+            <Route path="/product/:id" element={< ProductScreen />} />
+            <Route path="/cart" element={< CartScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
