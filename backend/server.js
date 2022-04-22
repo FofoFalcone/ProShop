a906a43c31c0db18e0ab8fcfd73d6e707ae56d1b
Alfonso Falcone <alfonso.falcone@webness.it>
2022-04-22 16:09:25 +0200

    Fetching all products from React with useEffect

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const products = require('./data/products')
 const app = express()
 
-app.listen(3000, console.log("Server running..."))
+app.listen(5000, console.log("Server running..."))
 
 // req si riferische alla richiesta fatta sulla base dell'URL
 // res è la risposta alla richiesta(req) che viene stampata nel browser
@@ -11,11 +11,11 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 })
 
-app.get('/products', (req, res) => {
+app.get('/api/products', (req, res) => {
     res.json(products);
 })
 
-app.get('/products/:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
     // Mostra in console l'id del prodotto
     console.log(req.params);
     const product = products.find((prod) => prod._id === req.params.id);
@@ -23,7 +23,7 @@ app.get('/products/:id', (req, res) => {
 })
 
 // Multiparametro
-app.get('/products/:id&:category', (req, res) => {
+app.get('/api/products/:id&:category', (req, res) => {
     // Mostra in console l'id del prodotto
     console.log(req.params);
     const product = products.find((prod) => prod._id === req.params.id && prod.category === req.params.category);
--- frontend/src/templates/HomeScreen.js
+++ frontend/src/templates/HomeScreen.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import ProductCard from "../components/ProductCard";
-import products from "../products";
+// import products from "../products";
+
+// aggiungiamo i prodotti come compoenent level-state
+// L'oggetto state è il punto in cui vengono archiviati i valori delle proprietà che appartengono al componente. 
+// Quando l'oggetto stato cambia, il componente esegue nuovamente il rendering.
+// products sarà uno state globale, ma per adesso lo renderemo parte di un componente
+// Axios ci servirà per effettuare le richieste HTTP da backend
+import axios from "axios";
 
 const HomeScreen = () => {
+  // Tra gli import all'inizio del documento impostiamo un componente useState e un componente useEffect
+  // { useState }
+  // In queste parentesi quadre inseriamo due cose
+  // il primo elemento è lo state (products)
+  // il secondo elemento è la funzione che si occuperà di manipolare o modificare lo state
+  // all'interno delle funzione useState possiamo definire un parametro [] per specificare che tipo di dato ci aspettiamo dalla nostra response
+  // Le parentesi quadre quindi, senza definire la funzione, non restituiranno risultati, la pagina sarà priva di contenuti e non ci saranno errori, perché non abbiamo indicato alcun dato
+  const [products, setProducts] = useState([])
+
+  // { useEffect }
+  // la funzione useEffect si avvia appena il componente viene caricato; qui dentro inseriremo la nostra HTTP Request
+  // All'interno, la funzione asincrona viene eseguita solo 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data)
+    }
+    fetchProducts();
+  }, [])
+
   return (
     <>
       <h1 className="my-4">Latest Products</h1>
